91229417554300493dbc5b2aa41440cedfc8b663
Luna Dahal <155752693+luna0-0@users.noreply.github.com>
2024-02-12 19:41:34 +0545

    Add files via upload

---
+++ 01_hello.py
@@ -0,0 +1,3 @@
+print("Hello World!!")
+import os
+print(os.listdir())
---
+++ 02_datatypes.py
@@ -0,0 +1,17 @@
+a=15
+b='Luna'
+c=12.486
+d=True
+e=None
+
+print(a)
+print(b)
+print(c)
+print(d)
+print(e)
+
+print(type(a))
+print(type(b))
+print(type(c))
+print(type(d))
+print(type(e))
