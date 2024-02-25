f83c46378715f36dd26c47a2f9a7d28da45c42e5
Luna Dahal <155752693+luna0-0@users.noreply.github.com>
2024-02-25 23:05:35 +0545

    Add files via upload

---
+++ 38_ClassObj.py
@@ -0,0 +1,8 @@
+class employee():
+    company="Microsoft"
+    def esalary(self):
+        print(f"Salary for the employee of {self.company} is {self.salary}")
+
+ram=employee()
+ram.salary=3000
+ram.esalary()
